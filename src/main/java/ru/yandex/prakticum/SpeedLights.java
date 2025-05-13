c2c706e2db240207c481bb8c532a857bcd4d566a
Denis R <Deoniska@me.com>
2025-05-13 08:59:26 +0300

    initial commit

---
+++ src/main/java/ru/yandex/prakticum/SpeedLights.java
@@ -0,0 +1,20 @@
+package ru.yandex.prakticum;
+
+public class SpeedLights {
+    /**
+     * Ограничение на скорость 60 км/ч
+     * если нет превышения - color green
+     * если превышение <= 20 - color yellow
+     * если превышение > 20 - color red
+     */
+
+    public String showLight(int currentLight) {
+        if (currentLight < 60) {
+            return "green";
+        } else if (currentLight < 80) {
+            return "yellow";
+        } else {
+            return "red";
+        }
+    }
+}
